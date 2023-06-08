f0c2982ed4ce058b4d53e225cf116e5ebc90de78
PrakharGarg <23prakhargarg2002@gmail.com>
2023-06-08 21:49:45 +0530

    feat(drag-n-drop):Add drag and drop between two divs

---
+++ index.js
@@ -0,0 +1,40 @@
+const dragContainer = document.querySelector('.drag-container');
+
+let draggableItems = document.querySelectorAll('.drag-item');
+
+for (let i = 0; i < draggableItems.length; i++){
+    draggableItems[i].setAttribute('id', i);
+}
+// Add event listeners to the the draggable items
+draggableItems.forEach((draggableItem) => {
+    draggableItem.addEventListener("dragstart", (e) => {
+        console.log("dragstart");// add the dragging class to the element being dragged.
+        e.target.classList.add("dragging");
+        // clear the drag data stored in the cache.
+        // e.dataTransfer.clearData();
+        // set new data to the drag 
+        e.dataTransfer.setData('text/plain', e.target.id);
+    });
+    draggableItem.addEventListener('dragend', (e) => {
+        e.target.classList.remove('dragging');// remove the dragging class after being dropped.
+    })
+}
+);
+const dropContainer = document.querySelector('.drop-container');
+dropContainer.addEventListener('dragover', (e) => {
+    console.log('dragging over');
+    e.preventDefault();// to allow drop feature.
+    
+})
+dropContainer.addEventListener('drop', function (e) {
+    
+    // fetches the id we set during the drag of the draggable elements
+    let draggedItemid = e.dataTransfer.getData('text/plain');
+    console.log(draggedItemid);
+    // Retrieve the dragged data
+    let draggedItem = document.getElementById(draggedItemid);
+    // draggedItem.classList.remove('dragging');
+    e.target.appendChild(draggedItem);
+})
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => document.location.reload())
